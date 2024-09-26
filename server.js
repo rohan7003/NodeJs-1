cd0216c1fc9ac1b1de0805d5cafb5da3947bf103
Rohan Patil <rohanrpatil7003@gmail.com>
2024-09-26 16:09:34 +0530

    done

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const port = 8081;
+http.createServer((req, res)=>{
+    res.writeHead(200, {"content-type":"text/html"});
+    res.write("<h5>we got our server here...</h5>");
+    res.end();
+})
+.listen(port,()=>{
+    console.log(`NodeJs Server Started Running On Port ${port}`);
+})
