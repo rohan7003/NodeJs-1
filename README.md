abab844e1cdb142cd83c7c6d7f120f87fecbf5af
Rohan Patil <rohanrpatil7003@gmail.com>
2024-09-30 11:08:22 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # NodeJs-1
+
+npm: node package manager
+
+Node Initilisation
+    >> npm init 
+
+To run application:
+    >> node server.js
+    >> npm start
+
+    >> npm run dev (nodemon)
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 8081;
 http.createServer((req, res)=>{
     res.writeHead(200, {"content-type":"text/html"});
-    res.write("<h5>we got our server here...</h5>");
+    res.write("<h5>we got our server here... checking nodemon</h5>");
     res.end();
 })
 .listen(port,()=>{
